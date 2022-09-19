0d07925fed39b9f33326e486e4dbe69435e2911c
melisaapoggilicenciada <112989336+melisaapoggilicenciada@users.noreply.github.com>
2022-09-18 21:49:29 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,29 @@
+let nombre = "Programación" + " " + "Fácil.";//Programación es facil
+let saludo = "Les doy la bienvenida a ";
+let año = "2022"
+let fraseCompleta = saludo+nombre+año;
+//se imprime la frase completa
+
+console.log(fraseCompleta);
+
+console.warn("hey!");
+alert("Mirar en la consola para obtener mas información");
+ 
+let frase = "JavaScript " + "es un lenguaje" + " " + "esencial para crear" + " " + "sitios web poderosos.";
+console.log(frase);
+
+console.log("❤🧡❤");
+
+
+let numeroString1 = "10";
+let numeroString2 = "20";
+let numeroString3 = "30";
+
+let concatenaNumeros = numeroString1 +" "+ numeroString2 + " "+ numeroString3;
+
+console.log(concatenaNumeros);
+var edad = prompt("Cual es tu edad?");
+console.log(edad);
+document.write("Mi edad es 30");
+console.warn("Verifique la edad antes de ingresar");
+
